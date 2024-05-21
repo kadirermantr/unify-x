070b6e1857f2453884f7396c2e4afa3cab8d0be0
kadirermantr <kadirermantr@gmail.com>
2024-05-21 07:58:14 +0300

    move authenticate function to service

--- app/Domain/Services/AuthService.php
+++ app/Domain/Services/AuthService.php
@@ -3,12 +3,23 @@
 namespace App\Domain\Services;
 
 use App\Domain\Contracts\AuthRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
 
 class AuthService
 {
     public function __construct(
         protected AuthRepositoryInterface $repository
     ) {
+    }
+
+    public function authenticate($username, $password): bool
+    {
+        $credentials = [
+            'email' => $username,
+            'password' => $password,
+        ];
+
+        return Auth::attempt($credentials);
     }
 
     public function create(array $data)
--- app/Http/Controllers/Api/IntegrationController.php
+++ app/Http/Controllers/Api/IntegrationController.php
@@ -2,29 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Domain\Services\AuthService;
 use App\Domain\Services\IntegrationService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\IntegrationStoreRequest;
 use App\Models\Integration;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 
 class IntegrationController extends Controller
 {
-    public function __construct(protected IntegrationService $service)
-    {
+    public function __construct(
+        protected AuthService $authService,
+        protected IntegrationService $integrationService,
+    ) {
     }
 
     public function store(IntegrationStoreRequest $request): JsonResponse
     {
-        if (! $this->authenticate($request->input('username'), $request->input('password'))) {
+        if (! $this->authService->authenticate($request->input('username'), $request->input('password'))) {
             return response()->json([
                 'error' => 'Email or password is wrong',
             ], 401);
         }
 
         $data = $request->validated();
-        $integration = $this->service->create($data);
+        $integration = $this->integrationService->create($data);
 
         return response()->json([
             'message' => 'Integration saved',
@@ -34,7 +36,7 @@ class IntegrationController extends Controller
 
     public function update(IntegrationStoreRequest $request, Integration $integration): JsonResponse
     {
-        $this->service->update($integration, $request->validated());
+        $this->integrationService->update($integration, $request->validated());
 
         return response()->json([
             'message' => 'Integration updated',
@@ -43,20 +45,10 @@ class IntegrationController extends Controller
 
     public function destroy(Integration $integration): JsonResponse
     {
-        $this->service->delete($integration);
+        $this->integrationService->delete($integration);
 
         return response()->json([
             'message' => 'Integration deleted',
         ]);
-    }
-
-    private function authenticate($username, $password): bool
-    {
-        $credentials = [
-            'email' => $username,
-            'password' => $password,
-        ];
-
-        return Auth::attempt($credentials);
     }
 }
